36101e7b0c4a8305c0d787de185e8d8cd85c4887
Matthew Lewis <matt@mplewis.com>
2015-02-24 13:20:12 -0600

    Add errors for firmware upload states

--- beansdk/src/main/java/com/punchthrough/bean/sdk/message/BeanError.java
+++ beansdk/src/main/java/com/punchthrough/bean/sdk/message/BeanError.java
@@ -5,9 +5,26 @@ package com.punchthrough.bean.sdk.message;
  */
 public enum BeanError {
     /**
-     * Timed out while waiting for state to update during programming, but before sending chunks
+     * Timed out during sketch programming, before sending chunks: Bean took too long to update its
+     * current state
      */
     STATE_TIMEOUT,
+    /**
+     * Timed out configuring OAD characteristics
+     */
+    OAD_CONFIG_TIMEOUT,
+    /**
+     * Timed out requesting current firmware version
+     */
+    CURR_FW_VER_TIMEOUT,
+    /**
+     * Timed out starting firmware download
+     */
+    FW_START_TIMEOUT,
+    /**
+     * Timed out while sending firmware packets
+     */
+    FW_DOWNLOAD_TIMEOUT,
     /**
      * Bean did not provide a reason for the error
      */
