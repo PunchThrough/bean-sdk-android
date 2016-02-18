a0163213a3bfc756883c2e8a29c3be78bc09f7aa
Stephen Stack <sstack@punchthrough.com>
2016-02-18 15:16:05 -0600

    style: clean up OADProfile and add class level doc

--- sdk/src/main/java/com/punchthrough/bean/sdk/internal/upload/firmware/FirmwareUploadState.java
+++ sdk/src/main/java/com/punchthrough/bean/sdk/internal/upload/firmware/FirmwareUploadState.java
@@ -5,18 +5,22 @@ public enum FirmwareUploadState {
      * No firmware upload is in progress
      */
     INACTIVE,
+
     /**
      * Waiting for notify to be enabled on OAD characteristics
      */
     AWAIT_NOTIFY_ENABLED,
+
     /**
      * Waiting for current OAD request header
      */
     AWAIT_CURRENT_HEADER,
+
     /**
      * Wrote the OAD response header, waiting for the device to accept a FW transfer
      */
     AWAIT_XFER_ACCEPT,
+
     /**
      * FW transfer accepted, currently sending blocks
      */
